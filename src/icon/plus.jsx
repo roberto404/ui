3ebc6812b0cf5216456add05fc907f8ebf64cc01
Gulyás Róbert <robert@1studio.hu>
2019-06-03 13:10:48 +0200

    chore(icon): remove unused icons

--- src/icon/plus.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default (props) =>
-(
-  <svg version="1.0" viewBox="0 0 24 24" { ...props } >
-    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
-  </svg>
-)
--- src/icon/star.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default (props) =>
-(
-  <svg version="1.0" viewBox="0 0 18 18" { ...props } >
-    <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
-  </svg>
-)
