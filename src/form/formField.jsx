7c2d8ee061751fd4fd01e24f6ed3b3df9dd516a4
Gulyás Róbert <robert@1studio.hu>
2019-09-02 22:01:57 +0200

    fix(form/plain): formfield extend plain

--- src/form/formField.jsx
+++ src/form/formField.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+// import { injectIntl } from 'react-intl';
 import isEqual from 'lodash/isEqual';
 import isEmpty from 'lodash/isEmpty';
 import classNames from 'classnames';
 
+
+/* !- Redux Actions */
+
 import { setValues } from './actions';
 
-import { FORM_SCHEME_KEY } from './constants';
 
-import { Plain } from './pure/intl';
+/* !- React Elements */
+
+import IconFalse from '../icon/mui/content/remove';
+import IconTrue from '../icon/mui/action/done';
+
+
+/* !- Constants */
+
+import { FORM_SCHEME_KEY } from './constants';
 
 
 /**
@@ -485,18 +496,83 @@ class FormField extends Component
       ...value.slice(index + 1)];
   };
 
+  renderPlainField()
+  {
+    const { intl, multipleData } = this.props;
+    const multipleDataText = () => intl ? intl.formatMessage({ id: multipleData }) : multipleData;
+    const placeholder =
+      Array.isArray(this.state.value) ? multipleDataText() : this.state.placeholder;
+
+    let value = this.state.value;
+    const data = this.getData();
+
+    // checkbox, radio
+    if (data.length)
+    {
+      const values = Array.isArray(value) ? value : [value];
+
+      value =
+        values
+          .map(v => data.find(({ id }) => id.toString() === v.toString()).title)
+          .join(', ');
+    }
+    // toggle, button
+    else if (typeof value === 'boolean')
+    {
+      // button data
+      if (typeof data[+value] !== 'undefined')
+      {
+        value = data[+value];
+      }
+      else
+      {
+        value = value ? <IconTrue className="w-1 h-1" /> : <IconFalse className="w-1 h-1" />;
+      }
+    }
+
+    return (
+      <div className={this.getClasses('plain')}>
+
+        { this.label }
+
+        <div className="table">
+
+          { this.state.prefix &&
+          <div className="prefix">{this.state.prefix}</div>
+          }
+
+          { this.props.dangerouslySetInnerHTML &&
+            <div dangerouslySetInnerHTML={{ '__html': value }} />
+          }
+          { !this.props.dangerouslySetInnerHTML &&
+            <div>{value || placeholder}</div>
+          }
+
+          { this.props.postfix &&
+          <div className="postfix">{this.state.postfix}</div>
+          }
+
+        </div>
+
+        { this.state.error &&
+          <div className="error">{this.state.error}</div>
+        }
+      </div>
+    );
+  }
+
   /**
    * This method is called when render the Component instance.
    * @return {ReactElement}
    */
   render()
   {
-    return !this.context.readOnly ? null :
-    (
-      <Plain
-        {...this.props}
-      />
-    );
+    if (this.context.readOnly !== true)
+    {
+      return null;
+    }
+
+    return this.renderPlainField();
   }
 }
 
--- src/form/pure/plain.jsx
+++ src/form/pure/plain.jsx
@@ -6,9 +6,6 @@ import React from 'react';
 
 import Field from '../formField';
 
-import IconFalse from '../../icon/mui/content/remove';
-import IconTrue from '../../icon/mui/action/done';
-
 
 /**
 * Input Component
@@ -17,8 +14,6 @@ import IconTrue from '../../icon/mui/action/done';
 */
 class Plain extends Field
 {
-  /* !- Renders */
-
   /**
    * This method is called when render the Component instance.
    * @override
@@ -26,67 +21,7 @@ class Plain extends Field
    */
   render()
   {
-    const { intl, multipleData } = this.props;
-    const multipleDataText = () => intl ? intl.formatMessage({ id: multipleData }) : multipleData;
-    const placeholder =
-      Array.isArray(this.state.value) ? multipleDataText() : this.state.placeholder;
-
-    let value = this.state.value;
-    const data = this.getData();
-
-    // checkbox, radio
-    if (data.length)
-    {
-      const values = Array.isArray(value) ? value : [value];
-
-      value =
-        values
-          .map(v => data.find(({ id }) => id.toString() === v.toString()).title)
-          .join(', ');
-    }
-    // toggle, button
-    else if (typeof value === 'boolean')
-    {
-      // button data
-      if (typeof data[+value] !== 'undefined')
-      {
-        value = data[+value];
-      }
-      else
-      {
-        value = value ? <IconTrue className="w-1 h-1" /> : <IconFalse className="w-1 h-1" />;
-      }
-    }
-
-    return (
-      <div className={this.getClasses('plain')}>
-
-        { this.label }
-
-        <div className="table">
-
-          { this.state.prefix &&
-          <div className="prefix">{this.state.prefix}</div>
-          }
-
-          { this.props.dangerouslySetInnerHTML &&
-            <div dangerouslySetInnerHTML={{ '__html': value }} />
-          }
-          { !this.props.dangerouslySetInnerHTML &&
-            <div>{value || placeholder}</div>
-          }
-
-          { this.props.postfix &&
-          <div className="postfix">{this.state.postfix}</div>
-          }
-
-        </div>
-
-        { this.state.error &&
-          <div className="error">{this.state.error}</div>
-        }
-      </div>
-    );
+    return this.renderPlainField();
   }
 }
 
