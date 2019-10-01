5d02236630b86c3a3a4d2b62d0835b204cb3f8d4
gidofalvi <gidofalvi.tamas@gmail.com>
2019-10-01 17:11:17 +0200

    fix(kontakt2): stock

--- engine/kontakt2/dev/src/views/stock/const.js
+++ engine/kontakt2/dev/src/views/stock/const.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FormattedNumber } from 'react-intl';
 
 import * as Filters from '@1studio/ui/grid/filters';
+import formatThousand from '@1studio/utils/string/formatThousand';
 
 /* !- Constants */
 
@@ -18,36 +19,39 @@ export const SETTINGS = {
         </div>
       ),
     },
-    rs2: {
-      title: <div className="text-left pl-4">RS2</div>,
+    rs: {
+      title: <div className="text-left pl-4">Összesen</div>,
       format: ({ record }) => (
         <div className="pl-3 relative">
-          {record.rs2 + record.rs21 > 0 && (
-            <div className="circle bg-green w-1 h-1 absolute pin-l m-1/2 ml-1" />
-          )}
-          <div>{record.rs2} db</div>
+          {parseInt(
+            record.rs2 +
+              record.rs21 +
+              record.rs22 +
+              record.rs6 +
+              record.rs61 +
+              record.rs62 +
+              record.rs8 +
+              record.rs81 +
+              record.rs82,
+          ) > 0 && <div className="circle bg-green w-1 h-1 absolute pin-l m-1/2 ml-1" />}
+          <div>{record.rs} db</div>
         </div>
       ),
+      align: 'left',
+    },
+    rs2: {
+      title: <div className="text-left pl-4">RS2</div>,
+      format: ({ record }) => `${record.rs2} db`,
       align: 'left',
     },
     rs6: {
       title: <div className="text-left pl-4">RS6</div>,
-      format: ({ record }) => (
-        <div className="pl-3 relative">
-          <div>{record.rs6} db</div>
-          <div className="text-s text-gray light">{record.rs61} db</div>
-        </div>
-      ),
+      format: ({ record }) => `${record.rs6} db`,
       align: 'left',
     },
     rs8: {
       title: <div className="text-left pl-4">RS8</div>,
-      format: ({ record }) => (
-        <div className="pl-3 relative">
-          <div>{record.rs8} db</div>
-          <div className="text-s text-gray light">{record.rs81} db</div>
-        </div>
-      ),
+      format: ({ record }) => `${record.rs8} db`,
       align: 'left',
     },
     r: {
@@ -58,17 +62,17 @@ export const SETTINGS = {
       title: 'Eladási ár',
       format: ({ record }) => (
         <div>
-          <div>
-            <FormattedNumber value={record.p1} /> Ft
-          </div>
+          <div>{`${formatThousand(record.p1)} Ft`}</div>
           {record.p1 !== record.p0 && (
-            <div className="text-s text-gray light">
-              <FormattedNumber value={record.p0} /> Ft
-            </div>
+            <div className="text-s text-gray light">{`${formatThousand(record.p0)} Ft`}</div>
           )}
         </div>
       ),
       width: '15%',
+    },
+    d: {
+      title: 'Méret',
+      format: ({ value }) => (value ? `${value} cm` : ''),
     },
   },
   paginate: {
