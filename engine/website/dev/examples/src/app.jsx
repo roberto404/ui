998551431f9677b9c33ce1e56e80edf919503d00
Gulyás Róbert <robert@1studio.hu>
2019-10-07 09:27:15 +0200

    docs(website): example

---
+++ engine/website/dev/examples/src/app.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+
+/* !- React Elements */
+
+import Layer from '@1studio/ui/layer';
+
+
+/* !- Example: Product */
+
+import Example from './product/thumbnail';
+
+
+
+
+export default () =>
+(
+  <div className="application">
+    <Example />
+    <Layer />
+  </div>
+);
