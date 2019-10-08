fd646c3b24c97cbcc26a63aebfaa1fd17b8bc70d
Gulyás Róbert <robert@1studio.hu>
2019-10-08 14:19:11 +0200

    fix(website): remove page

--- engine/website/dev/src/views/index.jsx
+++ engine/website/dev/src/views/index.jsx
@@ -1,11 +1,9 @@
-import Page from './page/';
 import Product from './product/product';
 import ProductGrid from './product/grid';
 import Cart from './cart/';
 import Favourites from './favourites/';
 
 export default {
-  Page,
   Product,
   ProductGrid,
   Favourites,
