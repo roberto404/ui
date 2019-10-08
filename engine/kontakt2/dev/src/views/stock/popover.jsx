9752a22f6f20cd0041de30e89cbb306b4c14dc44
gidofalvi <gidofalvi.tamas@gmail.com>
2019-10-08 20:05:11 +0200

    fix(kontakt2): stock

--- engine/kontakt2/dev/src/views/stock/popover.jsx
+++ engine/kontakt2/dev/src/views/stock/popover.jsx
@@ -10,29 +10,43 @@ import * as LayerActions from '@1studio/ui/layer/actions';
 const STORES = [2, 6, 8];
 
 const PopoverComponent = ({ record }) => (
-  <div className="pr-3" style={{ width: '250px' }}>
-    <div className="bold mb-1">Mintakészlet</div>
+  <div className="pr-3" style={{ width: '400px' }}>
+    <div className="flex">
+      <div className="column p-1 h-center v-center" style={{ width: '200px' }}>
+        <div className="p-1 m-1">&nbsp;</div>
+        <div className="p-1 m-1">Szabad készlet</div>
+        <div className="p-1 m-1">Gyártói minta</div>
+        <div className="p-1 m-1">Kivett készlet</div>
+      </div>
 
-    {STORES.map((store) =>
+      {STORES.map((store) =>
 {
-      const supply1 = record[`rs${store}1`];
-      const supply2 = record[`rs${store}2`];
-      const isSupply = parseInt(supply1) + parseInt(supply2) > 0;
-      return (
-        <div key={store} className="grid-2 h-center mb-1">
-          <div className="col-2-5">
+        const supply0 = record[`rs${store}`];
+        const supply1 = record[`rs${store}1`];
+        const supply2 = record[`rs${store}2`];
+        const isSupply = parseInt(supply0) + parseInt(supply1) + parseInt(supply2) > 0;
+        return (
+          // <div key={store} className="grid-2 h-center mb-1">
+          //   <div className="col-2-5">
+          //     <div
+          //       className={`p-1 bg-${isSupply ? 'green' : 'gray'} inline-block`}
+          //     >{`RS${store}`}</div>
+          //   </div>
+          //   <div className="col-1-5">{supply1}</div>
+          //   <div className="col-1-5">/</div>
+          //   <div className="col-1-5">{supply2}</div>
+          // </div>
+          <div className="column h-center v-center" style={{ width: '70px' }}>
             <div
-              className={`p-1 bg-${isSupply ? 'green' : 'gray'} inline-block`}
+              className={`p-1 m-1 bg-${isSupply ? 'green' : 'gray'} inline-block`}
             >{`RS${store}`}</div>
+            <div className="p-1 m-1">{supply0}</div>
+            <div className="p-1 m-1">{supply1}</div>
+            <div className="p-1 m-1">{supply2}</div>
           </div>
-          <div className="col-1-5">{supply1}</div>
-          <div className="col-1-5">/</div>
-          <div className="col-1-5">{supply2}</div>
-        </div>
-      );
-    })}
-
-    <div className="text-s">Gyártói minta / Kivett készlet</div>
+        );
+      })}
+    </div>
   </div>
 );
 
