3fce51cb18695f14ac1f23f745784dc092d619cc
Gulyás Róbert <robert@1studio.hu>
2020-01-13 19:15:19 +0100

    feat(Icon/fa):
    BREAKING CHANGE: CI

---
+++ src/icon/fa/facebookF.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * https://fontawesome.com/icons/facebook-f?style=brands
+ */
+export default props =>
+(
+  <svg version="1.0" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M17.5847,13.5l0.6666-4.3434h-4.1677V6.338c0-1.1883,0.5822-2.3466,2.4488-2.3466h1.8947
+	v-3.698c0,0-1.7194-0.2934-3.3633-0.2934c-3.4322,0-5.6756,2.0803-5.6756,5.8463v3.3103H5.573V13.5h3.8152V24h4.6955V13.5H17.5847z" />
+  </svg>
+);
