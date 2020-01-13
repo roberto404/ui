dfca2977240883b479783ee9bd8f8033a77f4090
Gulyás Róbert <robert@1studio.hu>
2020-01-13 18:57:23 +0100

    feat(Icon/FA):

---
+++ src/icon/fa/chevronLight.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * https://fontawesome.com/icons/chevron-up?style=light
+ */
+export default props =>
+(
+  <svg version="1.0" viewBox="0 0 448 512" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M4.465 366.475l7.07 7.071c4.686 4.686 12.284 4.686 16.971 0L224 178.053l195.494 195.493c4.686 4.686 12.284 4.686 16.971 0l7.07-7.071c4.686-4.686 4.686-12.284 0-16.97l-211.05-211.051c-4.686-4.686-12.284-4.686-16.971 0L4.465 349.505c-4.687 4.686-4.687 12.284 0 16.97z" />
+  </svg>
+);
