0ca23b84010d03425e8561e6aab96e27fb9e6488
Gulyás Róbert <robert@1studio.hu>
2020-06-01 20:23:13 +0200

    feat(layer/tooltip): isOnLayer

--- src/layer/tooltip.jsx
+++ src/layer/tooltip.jsx
@@ -4,27 +4,50 @@ import PropTypes from 'prop-types';
 
 import { flush, tooltip } from './actions';
 
-const Tooltip = ({ children, title }, { store }) =>
+const Tooltip = ({ className, children, title, onClick }, { store }) =>
 {
+  const onClickHandler = (event) =>
+  {
+    if (typeof onClick === 'function')
+    {
+      onClick(event);
+    }
+  }
 
   const onMouseHandler = (event) =>
   {
-    store.dispatch(tooltip(title, event));
+    const isLayer = store.getState().layer.active === true;
+
+    if (!isLayer)
+    {
+      store.dispatch(tooltip(title, event));
+    }
   };
 
   const onMouseOutHandler = (event) =>
   {
-    store.dispatch(flush());
+    const isTooltip = store.getState().layer.method === 'popover';
+
+    if (isTooltip)
+    {
+      store.dispatch(flush());
+    }
   };
 
   return (
     <div
+      className={className}
+      onClick={onClickHandler}
       onMouseEnter={onMouseHandler}
       onMouseOut={onMouseOutHandler}
     >
       {children}
     </div>
   )
+};
+
+Tooltip.defaultProps = {
+  className: 'inline-block',
 }
 
 
