a43ada52a52fb79370624f798d8cb20d4d8bcef8
Gulyás Róbert <robert@1studio.hu>
2020-06-01 20:25:13 +0200

    feat(calendar): const/DATETIME

--- src/calendar/constants.js
+++ src/calendar/constants.js
@@ -6,6 +6,7 @@ export const ONE_DAY = 24 * ONE_HOUR;
 export const Y_MOVEMENT_ACCURANCY = 15; // in minutes
 
 export const DATE_FORMAT = 'YYYY-MM-DD';
+export const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 export const DATE_FORMAT_HTML5 = 'YYYY-MM-DDTHH:mm';
 
 export const DATE_REGEX_HTML5 = /^20[0-2][0-9]-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]$/;
