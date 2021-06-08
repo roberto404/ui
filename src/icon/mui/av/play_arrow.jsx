76ab40f29d500f271c7ed92e9576ad182ac6d448
Gulyás Róbert <robert@1studio.hu>
2021-06-08 11:13:59 +0200

    feat(icon): av

---
+++ src/icon/mui/av/play_arrow.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>
+(
+  <svg version="1.0" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M8 5v14l11-7z" />
+  </svg>
+);
---
+++ src/icon/mui/av/videocam.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>
+(
+  <svg version="1.0" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4z" />
+  </svg>
+);
