552335ff85feeba02760cba47a4c470d006a6395
Gulyás Róbert <robert@1studio.hu>
2022-11-21 13:17:45 +0100

    feat(icon): mui

---
+++ src/icon/mui/action/expand.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>
+(
+  <svg version="1.0" viewBox="0 0 48 48" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M8.1 44v-3h31.8v3Zm16-4.5-7.6-7.6 2.15-2.15 3.95 3.95V14.3l-3.95 3.95-2.15-2.15 7.6-7.6 7.6 7.6-2.15 2.15-3.95-3.95v19.4l3.95-3.95 2.15 2.15ZM8.1 7V4h31.8v3Z" />
+  </svg>
+);
