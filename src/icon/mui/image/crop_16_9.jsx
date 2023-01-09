ea4bb0596f31520d6525125177a5008af7c17224
Gulyás Róbert <robert@1studio.hu>
2023-01-09 17:38:39 +0100

    feat(icon):

---
+++ src/icon/mui/image/crop_16_9.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>
+(
+  <svg version="1.0" viewBox="0 0 48 48" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M9 34q-1.2 0-2.1-.9Q6 32.2 6 31V17q0-1.2.9-2.1.9-.9 2.1-.9h30q1.2 0 2.1.9.9.9.9 2.1v14q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h30V17H9v14Zm0 0V17v14Z" />
+  </svg>
+);
