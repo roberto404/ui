eb09f10ae098d4619fcefbc865ed0188c56e9433
Gulyás Róbert <robert@1studio.hu>
2023-11-06 13:34:39 +0100

    feat: 🎸 auth
    reducer GETTERS extends

--- src/authentication/reducers.ts
+++ src/authentication/reducers.ts
@@ -1,4 +1,5 @@
 import Storage from '@1studio/utils/models/storage'; // 64
+import omit from 'lodash/omit';
 
 
 /**
@@ -63,5 +64,31 @@ export const GET_IS_LOGGED = ({ user }) =>
 
 export const GET_ID = ({ user }) =>
   user.id;
+
+export const GET_EMAIL = ({ user }) =>
+  user.email;
+
+export const GET_USERNAME = ({ user }) =>
+  user.username;
+
+export const GET_NAME = ({ user }) =>
+  user.name || `${user.firstName} ${user.lastName}`;
+
+export const GET_PERMISSION = ({ user }) =>
+  user.permission;
+
+export const GET_USER_DATA = ({ user }) => ({
+  name: user.name,
+  username: user.username,
+  firstName: user.firstName,
+  lastName: user.lastName,
+  phone: user.phone,
+  email: user.email,
+});
+
+export const GET_USER = ({ user }) =>
+  user;
+
+
 
 export default reducers;
