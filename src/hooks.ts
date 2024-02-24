2717bc017cdb8589c87602ee2e03a9c378e5354a
Gulyás Róbert <robert@1studio.hu>
2024-02-24 16:45:51 +0100

    feat: 🎸 hooks
    some useEffect hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,44 @@
-import { useReducer } from "react"
+import { useReducer, useEffect, EffectCallback, useRef } from "react"
 
-export const useForceUpdate = () =>
-{
+export const useComponentWillMount = (effect: EffectCallback) => {
+
+  const willMount = useRef(true)
+
+  if (willMount.current)
+    effect()
+
+  willMount.current = false
+}
+
+export const useComponentDidMount = (effect: EffectCallback) => {
+  useEffect(effect, [])
+}
+
+export const useHookUpdateProps = (callback, dependencies) => {
+
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const callbackWithProps = useRef((...args) => {
+    if (callbackRef.current) {
+      callbackRef.current(...args);
+    }
+  });
+
+  useEffect(() => {
+    callbackWithProps.current();
+  }, dependencies);
+
+  return (...args) => callbackWithProps.current(...args);
+}
+
+export const useForceUpdate = () => {
   return useReducer(() => ({}), {})[1] as () => void
+}
+
+export const useComponentWillUnmount = (effect: EffectCallback) => {
+  useEffect(() => effect, [])
 }
