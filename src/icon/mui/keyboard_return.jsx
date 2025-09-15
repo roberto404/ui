2796a6ba5a133328197d233b4a30ae6829a129fb
Gulyás Róbert <robert@1studio.hu>
2025-09-15 13:37:58 +0200

    feat(icon): add mui icons

---
+++ src/icon/mui/keyboard_return.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>
+(
+  <svg version="1.0" viewBox="0 -960 960 960" preserveAspectRatio="xMidYMid meet" {...props} >
+    <path d="M360-240 120-480l240-240 56 56-144 144h488v-160h80v240H272l144 144-56 56Z" />
+  </svg>
+);
